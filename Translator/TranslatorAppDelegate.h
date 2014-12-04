cd6255066c5c46a106d1d20cc6832fd90147f593
Robin <iyinrongbin@gmail.com>
2014-12-05 00:53:01 +0800

    Init

---
+++ Translator/TranslatorAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  TranslatorAppDelegate.h
+//  Translator
+//
+//  Created by Robin on 12/5/14.
+//  Copyright (c) 2014 Robin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TranslatorAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TranslatorTests/TranslatorTests.m
@@ -0,0 +1,40 @@
+//
+//  TranslatorTests.m
+//  TranslatorTests
+//
+//  Created by Robin on 12/5/14.
+//  Copyright (c) 2014 Robin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface TranslatorTests : XCTestCase
+
+@end
+
+@implementation TranslatorTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
